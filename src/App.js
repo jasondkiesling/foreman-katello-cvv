c01cf053d85324ea2e800de1a92fb317a7376ad0
Jason Kiesling <jason@jasondkiesling.com>
2020-02-13 13:10:42 -0500

    Create start of header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header.js";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="/foreman_helmet.svg"
+        alt="Helmet"
+        className="header-logo helmet"
+      />
+      <img src="/foreman_text.png" alt="Foreman" className="header-logo text" />
+    </header>
+  );
+}
