9e6917a19b67f1ee18947aeb5d5eb2e7d37a1728
Jason Kiesling <jason@jasondkiesling.com>
2020-02-21 12:36:10 -0500

    Login page and test component example

--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
-import Login from "./login/Login";
+import LoginPage from "./login/LoginPage";
 
 const router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
+      <Route path="/login" component={LoginPage} />
       {/* <Route path="/enhancement/:year/:enhancement" component={Enhancement} />
         <Route path="/error-404" component={error} />
         <Route path="/**" component={error} /> */}
