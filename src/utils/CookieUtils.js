710fb46ed39bf520830462ec739c9c5939c30813
Jason Kiesling <jason@jasondkiesling.com>
2020-02-27 20:45:24 -0500

    Add info for cookies

--- src/utils/CookieUtils.js
+++ src/utils/CookieUtils.js
@@ -14,6 +14,13 @@ export function getCookie(cookieName) {
   return "";
 }
 
+/*
+* @param {string} cookieName - The name of the cookie to be set
+* @param {string} cookieValue - The value of the cookie to be set
+* @param {number} expirationDays = How long the cookie should last.
+*               Leave as 0 for a session cookie or set as -1 to delete a cookie
+*/
+
 export function setCookie(cookieName, cookieValue, expirationDays = 0) {
   const d = new Date();
   d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
