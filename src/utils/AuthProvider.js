097604bf6ba9d7f90c8b375f43085c27ff17df32
Jason Kiesling <jason@jasondkiesling.com>
2020-02-27 20:20:29 -0500

    Check for auth, if no auth login

--- src/utils/AuthProvider.js
+++ src/utils/AuthProvider.js
@@ -1,15 +1,33 @@
 import React from "react";
 
+import { getCookie, setCookie } from "./CookieUtils";
+
 export const AuthContext = React.createContext();
 
 export default function AuthProvider({ children }) {
   const [basicAuth, setBasicAuth] = React.useState();
-  const setBasicAuthAndCookie = (basicAuthCode) => {
+  const [user, setUser] = React.useState({ firstName: "", lastName: "" });
+
+  const basicAuthCookieName = "basicAuthToken";
+  const setBasicAuthAndCookie = (basicAuthCode, cookieTimeLength = 0) => {
     setBasicAuth(basicAuthCode);
-    // set cookie
+    setCookie(basicAuthCookieName, basicAuthCode, cookieTimeLength);
   };
+
+  React.useEffect(() => {
+    if (window.location.pathname !== "/login" && !getCookie(basicAuthCookieName)) {
+      window.location.assign("/login");
+    } else if (window.location.pathname === "/login" && getCookie(basicAuthCookieName)) {
+      window.location.assign("/");
+    }
+    setBasicAuth(getCookie(basicAuthCookieName));
+  }, []);   //eslint-disable-line
+
   return (
-    <AuthContext.Provider value={{ basicAuth, setBasicAuth: setBasicAuthAndCookie }}>
+    <AuthContext.Provider value={{
+      basicAuth, setBasicAuth: setBasicAuthAndCookie, user, setUser,
+    }}
+    >
       {children}
     </AuthContext.Provider>
   );
