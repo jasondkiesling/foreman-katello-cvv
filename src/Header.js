469962ef1f8a480cb5ba7732eca5852dd646ada2
Jason Kiesling <jason@jasondkiesling.com>
2020-03-19 11:47:28 -0400

    Add initial header menu

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 import {
+  Dropdown,
+  DropdownToggle,
+  DropdownItem,
+} from "@patternfly/react-core";
+import { CaretDownIcon } from "@patternfly/react-icons";
+import "@patternfly/react-core/dist/styles/base.css";
+
+import {
   AuthContext, basicAuthCookieName, hostCookieName, usernameCookieName,
 } from "./utils/AuthProvider";
 import { setCookie } from "./utils/CookieUtils";
+
 
 import "./Header.css";
 
 export default function Header() {
   const { basicAuth } = React.useContext(AuthContext);
   const [user, setUser] = React.useState({ firstName: "", lastName: "" });
+  const [menuOpen, setMenuOpen] = React.useState(false);
 
   React.useEffect(() => {
     if (!basicAuth.basicAuth || !basicAuth.host || !basicAuth.username) {
@@ -40,8 +50,17 @@ export default function Header() {
         className="header-logo helmet"
       />
       <img src="/foreman_text.png" alt="Foreman" className="header-logo text" />
-      <p>{`${user.firstName} ${user.lastName}`}</p>
-      <button type="button" onClick={handleLogout}>logout</button>
+      <Dropdown
+        id="header-menu"
+        toggle={(
+          <DropdownToggle id="header-menu-toggle" onToggle={() => setMenuOpen(!menuOpen)} iconComponent={CaretDownIcon}>
+            {`${user.firstName} ${user.lastName}`}
+          </DropdownToggle>
+        )}
+        isOpen={menuOpen}
+      >
+        <DropdownItem key="logout" className="header-menu-item" onClick={() => { console.log("logout"); }}>Logout</DropdownItem>
+      </Dropdown>
     </header>
   );
 }
