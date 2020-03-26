930a8a5a88583a42fadb453ad57ebb6ba39f4f12
Kevin <kevinzhihuang@gmail.com>
2020-03-25 20:53:49 -0400

    CSS changes to make the page look prettier

--- src/CVV/CVVModals.js
+++ src/CVV/CVVModals.js
@@ -34,34 +34,46 @@ export default function CVVModals({ match }) {
   }, [basicAuth, match.params.id]);
 
   return (
-    <div id="environments">
-      {cvvs
-        ? cvvs.map((cvv) => (
-            <div key={cvv.id}>
-              {cvv.environments.map((cvv_env) => (
-                <Card isHoverable>
-                  <CardHeader>
-                    <strong>Version:&nbsp;</strong>
-                    {cvv.version}
-                  </CardHeader>
-                  <CardBody>
-                    <strong>Environment:&nbsp;</strong>
-                    {`${cvv_env.name}`}
-                  </CardBody>
-                  <CardBody>
-                    <strong>Description:&nbsp;</strong>
-                    {cvv.description}
-                  </CardBody>
-                </Card>
-              ))}
-            </div>
-          ))
-        : null}
-      <h3 id="NLE">No Lifecycle Environment</h3>
-      <h3 id="Library">Library</h3>
-      <h3 id="Testing">Testing</h3>
-      <h3 id="Development">Development</h3>
-      <h3 id="Production">Production</h3>
+    <div id="environments" class="pf-l-stack">
+      <div id="NLE" class="pf-l-stack__item">
+        <div class="env_titles">No Lifecycle Environment</div>
+      </div>
+      <div class="pf-l-stack__item">
+        {cvvs
+          ? cvvs.map((cvv) => (
+              <div key={cvv.id} id="cards">
+                {cvv.environments.map((cvv_env) => (
+                  <Card isHoverable>
+                    <CardHeader>
+                      <strong>Version:&nbsp;</strong>
+                      {cvv.version}
+                    </CardHeader>
+                    <CardBody>
+                      <strong>Environment:&nbsp;</strong>
+                      {`${cvv_env.name}`}
+                    </CardBody>
+                    <CardBody>
+                      <strong>Description:&nbsp;</strong>
+                      {cvv.description}
+                    </CardBody>
+                  </Card>
+                ))}
+              </div>
+            ))
+          : null}
+      </div>
+      <div id="Library" class="pf-l-stack__item">
+        <div class="env_titles">Library</div>
+      </div>
+      <div id="Testing" class="pf-l-stack__item">
+        <div class="env_titles">Testing</div>
+      </div>
+      <div id="Development" class="pf-l-stack__item">
+        <div class="env_titles">Development</div>
+      </div>
+      <div id="Production" class="pf-l-stack__item">
+        <div class="env_titles">Production</div>
+      </div>
     </div>
   );
 }
