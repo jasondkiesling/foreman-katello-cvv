a96d2cef4dd2d6b50fa33d5a86d7bf077d828f81
Evan O'Malley <eeomaly@gmail.com>
2020-05-01 14:48:10 -0400

    add more info to cvv modals

--- src/CVV/CVVModal.js
+++ src/CVV/CVVModal.js
@@ -66,14 +66,21 @@ export default function CVVModal({ title, open, onClose, cvvID, orgID }) {
       onClose={onClose}
       envs={orgEnvironments}
     >
-      {cvvInfo.description ? `${cvvInfo.description}` : `No description.`}
+      {(cvvInfo.description) ? `${cvvInfo.description}` : `No description.`}
       <br /> <br />
-      {`Packages:\t${cvvInfo.package_count}`}
+      {/* {(cvvInfo.repositories.length === 0) ? null : `Repos:\t\t${cvvInfo.repositories.length}`} */}
+      {(cvvInfo.package_group_count) ? <p>{`Package Groups:\t${cvvInfo.package_group_count}`}</p> : null}
+      {(cvvInfo.package_count) ? <p>{`Packages:\t${cvvInfo.package_count}`}</p> : null}
+      {(cvvInfo.file_count) ? <p>{`Files:\t\t${cvvInfo.file_count}`}</p> : null}
+      {(cvvInfo.srpm_count) ? <p>{`RPMs:\t\t${cvvInfo.srpm_count}`}</p> : null}
+      {(cvvInfo.module_stream_count) ? <p>{`Module Streams:\t${cvvInfo.module_stream_count}`}</p> : null}
+      {(cvvInfo.puppet_module_count) ? <p>{`Puppet Modules:\t${cvvInfo.puppet_module_count}`}</p> : null}
+      {(cvvInfo.docker_manifest_count) ? <p>{`Docker Manifests:\t${cvvInfo.docker_manifest_count}`}</p> : null}
+      {(cvvInfo.docker_manifest_list_count) ? <p>{`Docker Manifest Lists:\t${cvvInfo.docker_manifest_list_count}`}</p> : null}
+      {(cvvInfo.docker_tag_count) ? <p>{`Docker Tags:\t${cvvInfo.docker_tag_count}`}</p> : null}
+      {(cvvInfo.ostree_branch_count) ? <p>{`OSTree Branches:\t${cvvInfo.ostree_branch_count}`}</p> : null}
+      {(cvvInfo.deb_count) ? <p>{`Debian Repos:\t${cvvInfo.deb_count}`}</p> : null}
       <br />
-      {`Files:\t\t${cvvInfo.file_count}`}
-      <br />
-      {`RPMs:\t\t${cvvInfo.srpm_count}`}
-      <br /> <br />
       <PromotionDropdown cvvID={cvvID} envs={orgEnvironments} />
       {/* <Button className="remove-button" onClick={handleRemoveClick}>
         Remove
